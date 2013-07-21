697de9daaec62608a26c4e57f6bf4c3da70a7968
Hsin-Hsiang Huang <katehuang0320@gmail.com>
2013-07-21 15:49:50 +0800

    Lottery Helpers! :muscle

---
+++ lib/lottery/helper.rb
@@ -0,0 +1,26 @@
+module Lottery
+  module LotteryHelper
+    def draw(max=0, amount=0)
+      [*1..max].sample(amount).sort
+    end
+
+    def make_hash(arr1, arr2=nil)
+      { first: arr1, second: arr2 }
+    end
+  end
+
+  module LotteryPPrint
+    def pprint(first_name: 'Numbers', second_name: 'Lucky Numbers')
+      printf "%s: %s\t", first_name, self[:first]
+      if self[:second].nil?
+        puts
+      else
+        printf "%s: %s\n", second_name, self[:second]
+      end
+    end
+  end
+end
+
+class Hash
+  include Lottery::LotteryPPrint
+end
