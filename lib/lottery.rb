6bc5f3f57148ba85ae8db379fd725e495c597b82
Hsin-Hsiang Huang <katehuang0320@gmail.com>
2013-07-21 15:47:03 +0800

    version 0.0.1 :rocket'

---
+++ lib/lottery.rb
@@ -0,0 +1,10 @@
+require "lottery/version"
+require "lottery/helper"
+require "lottery/europe"
+require "lottery/taiwan"
+require "lottery/spain"
+require "lottery/command"
+
+module Lottery
+
+end
---
+++ lib/lottery/version.rb
@@ -0,0 +1,3 @@
+module Lottery
+  VERSION = "0.0.1"
+end
